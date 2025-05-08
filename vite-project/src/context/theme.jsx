ec207035772b2e22ca75dc725099ffc73fc580ee
FearSmoker <148282674+FearSmoker@users.noreply.github.com>
2025-05-08 13:21:32 +0530

    Light/Dark Theme

---
+++ vite-project/src/context/theme.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext({
+  themeMode: "light",
+  darkTheme: () => {},
+  lightTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }) => {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => setThemeMode("light");
+  const darkTheme = () => setThemeMode("dark");
+
+  useEffect(() => {
+    const root = document.querySelector('html');
+    root.classList.remove("light", "dark");
+    root.classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeContext.Provider value={{ themeMode, lightTheme, darkTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default function useTheme() {
+  return useContext(ThemeContext);
+}
