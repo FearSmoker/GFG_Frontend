4949a077acf4997fec6ded06f4a435243533473c
Sneha Tomar <snehatomar8612@gmail.com>
2025-06-06 15:25:37 +0530

    Update AboutUs.jsx

--- vite-project/src/pages/AboutUs.jsx
+++ vite-project/src/pages/AboutUs.jsx
@@ -1,66 +1,56 @@
-import GradientBox from "../components/GradientBox";
-import NeonText from "../components/NeonText";
-const AboutUs=()=>{
-    return (
-      <div>
-        <div className="bg-[#000000] min-h-screen text-white flex flex-col justify-center items-center gap-10 ">
-          <div className="flex gap-5 justify-center mt-5 ">
-            <NeonText text="ABOUT" color="text-white underline" />
-            <NeonText text="US" color="text-emerald-500 underline" />
-          </div>
-          <p className="text-center px-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            debitis, cupiditate voluptate fugiat perspiciatis totam. Dolores
-            magnam ex neque quas sit facere aperiam deserunt dolorem consequuntur,
-            quo, nulla itaque aut! Lorem ipsum dolor sit amet consectetur
-            adipisicing nemo incidunt blanditiis tempore commodi, earum
-            nostrum quisquam saepe ab facilis, atque perspiciatis consequuntur
-            quos ducimus ipsam tempora quibusdam fugit rerum. Lorem ipsum dolor
-            sit amet consectetur adipisicing elit. Et repudiandae minus,
-            accusamus, doloremque libero rem quam a nemo quo minima fugit omnis
-            placeat dolor quia beatae reiciendis quis ea est?Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Tempora debitis, cupiditate
-            voluptate fugiat perspiciatis totam. Dolores magnam ex neque quas sit
-            facere aperiam deserunt dolorem consequuntur, quo, nulla itaque aut!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo
-            incidunt blanditiis tempore commodi, earum nostrum quisquam saepe ab
-            facilis, atque perspiciatis consequuntur quos ducimus ipsam tempora
-            quibusdam fugit rerum. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Et repudiandae minus, accusamus, doloremque libero
-            rem quam a nemo quo minima fugit omnis placeat dolor quia beatae
-            reiciendis quis ea est?
+import React from 'react';
+
+const AboutUs = () => {
+  const cardStyle =
+    "bg-[#092C4C] text-white p-6 rounded-xl w-full sm:w-[30%]";
+
+  const headingStyle = "text-[#00FFFF] text-2xl font-semibold mb-4 text-center";
+
+  const paragraphStyle = "text-sm text-gray-200 text-justify";
+
+  return (
+    <div className="min-h-screen bg-[#071B2B] flex flex-col items-center justify-center px-4 py-10">
+      <h2 className="text-4xl font-bold text-[#00FFBF] mb-10">About Us</h2>
+      <div className="flex flex-col sm:flex-row gap-6 justify-center items-start w-full max-w-6xl">
+        <div className={cardStyle}>
+          <h3 className={headingStyle}>Who are we?</h3>
+          <p className={paragraphStyle}>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+            faucibus ex sapien vitae pellentesque sem placerat. In id cursus
+            mi pretium tellus duis convallis. Tempus leo eu aenean sed diam
+            urna tempor. Pulvinar varius fringilla lacus nec metus bibendum
+            egestas. Iaculis massa nisi malesuada lacinia integer nunc posuere.
+            Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora
+            torquent per conubia nostra inceptos himenaeos.
           </p>
-          <p className="text-center px-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            debitis, cupiditate voluptate fugiat perspiciatis totam. Dolores
-            magnam ex neque quas sit facere aperiam deserunt dolorem consequuntur,
-            quo, nulla itaque aut! Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Esse nemo incidunt blanditiis tempore commodi, earum
-            nostrum quisquam saepe ab facilis, atque perspiciatis consequuntur
-            quos ducimus ipsam tempora quibusdam fugit rerum. Lorem ipsum dolor
-            sit amet consectetur adipisicing elit. Et repudiandae minus,
-            accusamus, doloremque libero rem quam a nemo quo minima fugit omnis
-            placeat dolor quia beatae reiciendis quis ea est? Lorem ipsum dolor
-            sit amet consectetur adipisicing elit. Tempora debitis, cupiditate
-            voluptate fugiat perspiciatis totam. Dolores magnam ex neque quas sit
-            facere aperiam deserunt dolorem consequuntur, quo, nulla itaque aut!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse nemo
-            incidunt blanditiis tempore commodi, earum nostrum quisquam saepe ab
-            facilis, atque perspiciatis consequuntur quos ducimus ipsam tempora
-            quibusdam fugit rerum. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Et repudiandae minus, accusamus, doloremque libero
-            rem quam a nemo quo minima fugit omnis placeat dolor quia beatae
-            reiciendis quis ea est?
+        </div>
+        <div className={cardStyle}>
+          <h3 className={headingStyle}>Our Vision</h3>
+          <p className={paragraphStyle}>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+            faucibus ex sapien vitae pellentesque sem placerat. In id cursus
+            mi pretium tellus duis convallis. Tempus leo eu aenean sed diam
+            urna tempor. Pulvinar varius fringilla lacus nec metus bibendum
+            egestas. Iaculis massa nisi malesuada lacinia integer nunc posuere.
+            Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora
+            torquent per conubia nostra inceptos himenaeos.
           </p>
-          <div className="w-[80%] max-w-[77.625rem] h-[0.01rem] bg-white mx-auto"></div>
-          <a
-            className="pb-5 underline"
-            href="https://mail.google.com/mail/?view=cm&to=geeksforgeekmits@gmail.com" target="_blank"
-          >
-            <GradientBox text="&nbsp;geeksforgeekmits@gmail.com&nbsp;" />
-          </a>
+        </div>
+        <div className={cardStyle}>
+          <h3 className={headingStyle}>Our Mission</h3>
+          <p className={paragraphStyle}>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
+            faucibus ex sapien vitae pellentesque sem placerat. In id cursus
+            mi pretium tellus duis convallis. Tempus leo eu aenean sed diam
+            urna tempor. Pulvinar varius fringilla lacus nec metus bibendum
+            egestas. Iaculis massa nisi malesuada lacinia integer nunc posuere.
+            Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora
+            torquent per conubia nostra inceptos himenaeos.
+          </p>
         </div>
       </div>
-    );
-}
-export default AboutUs
+    </div>
+  );
+};
+
+export default AboutUs;
