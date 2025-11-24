53e60189aecf2e4a4a69eb6d1856e61300433b95
Naitikk-J <24ir10na46@mitsgwl.ac.in>
2025-11-24 13:59:35 +0530

    minor changes to fix route and component names

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -176,8 +176,8 @@ const Navbar = () => {
             />
             <NavbarLink
               label="GFG-Connect"
-              onClick={() => goTo("/nation-skill-up")}
-              active={currentPath === "//nation-skill-up"}
+              onClick={() => goTo("/GFG-Connect")}
+              active={currentPath === "/GFG-Connect"}
             />
             <NavbarLink
               label="POTD"
@@ -447,8 +447,8 @@ const Navbar = () => {
                   },
                   { label: "Events", path: "/events", icon: "🎯" },
                   {
-                    label: "Nation Skill Up!",
-                    path: "/nation-skill-up",
+                    label: "GFG-Connect",
+                    path: "/GFG-Connect",
                     icon: "🚀",
                   },
                   { label: "About Us", path: "/about-us", icon: "ℹ️" },
